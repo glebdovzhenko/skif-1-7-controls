171cbe9c69492103654a78b1cb270d8310ae39e9
Gleb Dovzhenko <dovjenko.g@gmail.com>
2026-03-30 19:41:27 +0700

    Spock launches and runs sar_demo

--- run-spock.sh
+++ run-spock.sh
@@ -1,2 +1,1 @@
-#docker run -it --network=skif-1-7-net -e TANGO_HOST=tango-dbds:10000 sardana-server /home/tango/pytango-env/bin/spock 
-docker run -it --network=skif-1-7-net -e TANGO_HOST=tango-dbds:10000 sardana-server spock 
+docker run -it --network=skif-1-7-net -e TANGO_HOST=tango-dbds:10000 sardana-server spock
