f236c5ee6005401a52c8ab31cf43f90311edcecd
Gleb Dovzhenko <dovjenko.g@gmail.com>
2026-04-19 18:13:24 +0700

    Trying to set up docker on Windows

---
+++ run-jive.bat
@@ -0,0 +1,2 @@
+set TANGO_HOST=127.0.0.1:10000
+java -jar bin\Jive-7.46-jar-with-dependencies-openjdk-17.jar
---
+++ run-spock.bat
@@ -0,0 +1,1 @@
+docker run -it --network=skif-1-7-net -e TANGO_HOST=tango-dbds:10000 sardana-server spock
